0b5b89bade2aab5d9baf7a235107e6f60ee59e29
Masakazu_Harada <seungri.uhm@gmail.com>
2023-08-01 21:47:04 +0900

    modelスペックの練習まで

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  describe "Book#title_with_author" do
+    context "Book#titleが文字列のとき" do
+      let(:book){ Book.new(title: "RubyBook", author: "matz")}
+      it "titleとauthorを結んだ文字列が返ること" do
+        expect(book.title_with_author).to eq("RubyBook - matz")
+      end
+    end
+    context "Book#titleがnilのとき" do
+      it "空のtitleとauthorを結んだ文字列が返ること" do
+        book = Book.new(author: "matz")
+        expect(book.title_with_author).to eq(" - matz")
+      end
+    end
+  end
+end
